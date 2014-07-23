2f99fffba7dcf17e889e3c28a7df1db136d7ca71
tminglei <tmlneu@gmail.com>
2014-07-23 09:56:15 +0800

    simplify codes and user interfaces

--- index.js
+++ index.js
@@ -4,76 +4,49 @@ var path = require('path');
 var utils = require('./utils');
 var _  = require('lodash-node');
 
-function _mkOptions(action, options, names) {
-	if (names && !_(names).isArray()) {
-		options = names;
-		names = null;
-	}
-	options = options || {};
-	options.action  = action;
-	options.include = options.include || names;
-	return options;
-}
+module.exports = function (browserify, options) {
+	options = options || { "require": utils.componentNames() };
 
-function brbower(browserify, options) {
-	options = options || {};
-	
-	var workinglist = _(options.include || utils.componentNames())
-		// filter out excluded names
-		.filter(function(name) {
-			var name0 = name.split(':')[0];
-			return !_(options.exclude).map(function(name1) {
-				return name1.split(':')[0];
-			}).contains(name0);
-		})
-		// merge in alias configs
-		.map(function(name) {
-			var found = _(options.alias).find(function(name1) {
-				return name.split(':')[0] === name1.split(':')[0];
+	_(options).forEach(function(conf, action) {
+		if (_(conf).isArray()) conf = { "include": conf };
+
+		var workinglist = _(conf.include || utils.componentNames())
+			// filter out excluded names
+			.filter(function(name) {
+				return !_(conf.exclude).map(function(name1) {
+					return name1.split(':')[0];
+				}).contains(name.split(':')[0]);
+			})
+			// merge in alias configs
+			.map(function(name) {
+				var found = _(conf.alias).find(function(name1) {
+					return name.split(':')[0] === name1.split(':')[0];
+				});
+				return found || name;
+			})
+			// prepare the working list
+			.map(function(rawname) {
+				var name = rawname.split(':')[0],
+					alias = rawname.split(':')[1];
+				return {
+					name:  name,
+					alias: alias || name,
+					path: utils.resolve(name)
+				};
 			});
-			return found || name;
-		})
-		// prepare the working list
-		.map(function(rawname) {
-			var name = rawname.split(':')[0],
-				alias = rawname.split(':')[1];
-			return {
-				name: name,
-				alias: alias || name,
-				path: utils.resolve(name)
-			}
-		});
 
-	options.action = options.action || 'require';
-	if (options.action === 'require') {
-		workinglist.forEach(function(item) {
-			browserify.require(item.path, { expose: item.alias });
-		});
-	} else { // external
-		workinglist.forEach(function(item) {
-			browserify.external(item.alias);
-		});
-	}
-}
+		if (!_(['require', 'external']).contains(action)) {
+			throw new Error('unsupportted action: ' + action);
+		}
 
-///
-module.exports = brbower;
-
-module.exports.require = function(names, options) {
-	return function(browserify) {
-		brbower(browserify, _mkOptions('require', options, names));
-	}
-};
-
-module.exports.external = function(names, options) {
-	return function(browserify) {
-		brbower(browserify, _mkOptions('external', options, names));
-	}
-};
-
-module.exports.resolve = function(name) {
-	if(!_(utils.componentNames()).contains(utils.componentName(name))) {
-		throw new Error('Can\'t find bower component for: ' + name);
-	}
-	return utils.resolve(name);
+		if (action === 'require') {
+			workinglist.forEach(function(item) {
+				browserify.require(item.path, { expose: item.alias });
+			});
+		} else { // external
+			workinglist.forEach(function(item) {
+				browserify.external(item.alias);
+			});
+		}
+	});
 };
